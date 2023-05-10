b5a088058bbc84b903bd165f80d3f6210c6dea29
Jens Krüger <jens.krueger@uni-due.de>
2023-05-10 22:54:34 +0200

    add grid view

--- 03_MS/main.cpp
+++ 03_MS/main.cpp
@@ -5,19 +5,54 @@
 class MyGLApp : public GLApp {
 public:
   std::vector<float> data;
+  std::vector<float> grid;
   Image image = BMP::load("image.bmp");
   std::vector<uint8_t> isovalues{128};
   bool useAsymptoticDecider{true};
   bool doLinearSampling{true};
+  bool drawGridLines{false};
   
   virtual void init() override {
     glEnv.setTitle("Marching Squares demo");
     GL(glDisable(GL_CULL_FACE));
     GL(glDisable(GL_DEPTH_TEST));
     GL(glClearColor(0,0,0,0));
+    GL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    GL(glBlendEquation(GL_FUNC_ADD));
+    GL(glEnable(GL_BLEND));
     extractIsoline();
+    generateGridLines();
   }
-  
+
+  void generateGridLines() {
+    for (size_t y = 0;y<image.height-1;++y) {
+      grid.push_back(-1); grid.push_back(y/float(image.height-1)*2-1);
+      grid.push_back(0);
+
+      grid.push_back(1.0f); grid.push_back(1.0f);
+      grid.push_back(0.0f); grid.push_back(0.2f);
+
+      grid.push_back(1); grid.push_back(y/float(image.height-1)*2-1);
+      grid.push_back(0);
+
+      grid.push_back(1.0f); grid.push_back(1.0f);
+      grid.push_back(0.0f); grid.push_back(0.2f);
+    }
+    for (size_t x = 0;x<image.width-1;++x) {
+      grid.push_back(x/float(image.width-1)*2-1); grid.push_back(-1);
+      grid.push_back(0);
+
+      grid.push_back(1.0f); grid.push_back(1.0f);
+      grid.push_back(0.0f); grid.push_back(0.2f);
+
+      grid.push_back(x/float(image.width-1)*2-1); grid.push_back(1);
+      grid.push_back(0);
+
+      grid.push_back(1.0f); grid.push_back(1.0f);
+      grid.push_back(0.0f); grid.push_back(0.2f);
+    }
+  }
+
   void extractIsoline() {
     data.clear();
     for (const uint8_t isovalue : isovalues) {
@@ -38,6 +73,7 @@ public:
   virtual void draw() override {
     GL(glClear(GL_COLOR_BUFFER_BIT));
     drawImage(image,{-1,-1},{1,1},true);
+    if (drawGridLines) drawLines(grid, LineDrawType::LIST, 2);
     drawLines(data, LineDrawType::LIST, 2);
   }
   
@@ -51,6 +87,9 @@ public:
           useAsymptoticDecider = ! useAsymptoticDecider;
           std::cout << "Asymptotic Decider is " << (useAsymptoticDecider ? "enabled" : "disabled") << std::endl;
           extractIsoline();
+          break;
+        case GLFW_KEY_G:
+          drawGridLines = ! drawGridLines;
           break;
         case GLFW_KEY_F:
           doLinearSampling = ! doLinearSampling;
