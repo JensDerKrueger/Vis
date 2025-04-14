c8587619c56f184da78f8b602aa709d97cbf4d4c
Jens Krüger <jens.krueger@uni-due.de>
2025-04-14 09:40:44 +0200

    add toggle for smaller dataset, remove GLFW consts

--- 03_MS/main.cpp
+++ 03_MS/main.cpp
@@ -6,7 +6,8 @@ class MyGLApp : public GLApp {
 public:
   std::vector<float> data;
   std::vector<float> grid;
-  Image image = BMP::load("image.bmp");
+  uint8_t currentImage{1};
+  Image images[2] = {BMP::load("image.bmp"), BMP::load("image_small.bmp")};
   std::vector<uint8_t> isovalues{128};
   bool useAsymptoticDecider{true};
   bool doLinearSampling{true};
@@ -25,8 +26,11 @@ public:
   }
 
   void generateGridLines() {
-    for (size_t y = 0;y<image.height-1;++y) {
-      const float fy = y/float(image.height-1)*2-1;
+    grid.clear();
+    const float xStagger = 0.5f/float(images[currentImage].width);
+    const float yStagger = 0.5f/float(images[currentImage].height);
+    for (size_t y = 0;y<images[currentImage].height;++y) {
+      const float fy = (y/float(images[currentImage].height)+yStagger)*2-1;
       grid.push_back(-1); grid.push_back(fy); grid.push_back(0);
       grid.push_back(1.0f); grid.push_back(1.0f);
       grid.push_back(0.0f); grid.push_back(0.2f);
@@ -35,8 +39,8 @@ public:
       grid.push_back(1.0f); grid.push_back(1.0f);
       grid.push_back(0.0f); grid.push_back(0.2f);
     }
-    for (size_t x = 0;x<image.width-1;++x) {
-      const float fx = x/float(image.width-1)*2-1;
+    for (size_t x = 0;x<images[currentImage].width;++x) {
+      const float fx = (x/float(images[currentImage].width)+xStagger)*2-1;
       grid.push_back(fx); grid.push_back(-1); grid.push_back(0);
       grid.push_back(1.0f); grid.push_back(1.0f);
       grid.push_back(0.0f); grid.push_back(0.2f);
@@ -50,7 +54,7 @@ public:
   void extractIsoline() {
     data.clear();
     for (const uint8_t isovalue : isovalues) {
-      Isoline s{image, isovalue, useAsymptoticDecider};
+      Isoline s{images[currentImage], isovalue, useAsymptoticDecider};
       for (const Vec2& v : s.vertices) {
         data.push_back(v[0]);
         data.push_back(v[1]);
@@ -66,48 +70,52 @@ public:
   
   virtual void draw() override {
     GL(glClear(GL_COLOR_BUFFER_BIT));
-    drawImage(image,{-1,-1},{1,1},true);
+    drawImage(images[currentImage]);
     if (drawGridLines) drawLines(grid, LineDrawType::LIST, 2);
     drawLines(data, LineDrawType::LIST, 2);
   }
   
   virtual void keyboard(int key, int scancode, int action, int mods) override {
-    if (action == GLFW_PRESS) {
+    if (action == GLENV_PRESS) {
       switch (key) {
-        case GLFW_KEY_ESCAPE:
+        case GLENV_KEY_ESCAPE:
           closeWindow();
           break;
-        case GLFW_KEY_D:
+        case GLENV_KEY_D:
           useAsymptoticDecider = ! useAsymptoticDecider;
           std::cout << "Asymptotic Decider is " << (useAsymptoticDecider ? "enabled" : "disabled") << std::endl;
           extractIsoline();
           break;
-        case GLFW_KEY_G:
+        case GLENV_KEY_G:
           drawGridLines = ! drawGridLines;
           break;
-        case GLFW_KEY_F:
+        case GLENV_KEY_F:
           doLinearSampling = ! doLinearSampling;
           setImageFilter(doLinearSampling ? GL_LINEAR : GL_NEAREST,
                          doLinearSampling ? GL_LINEAR : GL_NEAREST);
           break;
-        case GLFW_KEY_ENTER:
+        case GLENV_KEY_ENTER:
           isovalues.push_back(isovalues.back());
           extractIsoline();
           break;
-        case GLFW_KEY_C:
+        case GLENV_KEY_T:
+          currentImage = 1 - currentImage;
+          [[fallthrough]];
+        case GLENV_KEY_C:
           isovalues.clear();
           isovalues.push_back(128);
           extractIsoline();
+          generateGridLines();
           break;
-
       }
     }
+
     switch (key) {
-      case GLFW_KEY_UP:
+      case GLENV_KEY_UP:
         isovalues[isovalues.size()-1]++;
         extractIsoline();
         break;
-      case GLFW_KEY_DOWN:
+      case GLENV_KEY_DOWN:
         isovalues[isovalues.size()-1]--;
         extractIsoline();
         break;
