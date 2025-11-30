1fd354769c0d925fe122e688375e0c3a809b4507
Jens Krüger <jens.krueger@uni-due.de>
2025-11-30 15:00:43 +0300

    use vector based transfer function

--- 04_DVR/cubeFS.glsl
+++ 04_DVR/cubeFS.glsl
@@ -4,18 +4,15 @@ in vec3 entryPoint;
 out vec4 result;
 
 uniform sampler3D volume;
+uniform sampler1D transfer;
 
-uniform float smoothStepStart;
-uniform float smoothStepWidth;
 uniform float oversampling;
-uniform vec3 voxelCount;
 uniform vec3 cameraPosInTextureSpace;
 uniform vec3 minBounds;
 uniform vec3 maxBounds;
 
 vec4 transferFunction(float v) {
-  v = clamp((v - smoothStepStart) / (smoothStepWidth), 0.0, 1.0);
-  return vec4(v*v * (3-2*v));
+  return texture(transfer, v);
 }
 
 vec4 under(vec4 current, vec4 last) {
@@ -26,27 +23,20 @@ vec4 under(vec4 current, vec4 last) {
 
 bool inBounds(vec3 pos) {
   return pos.x >= minBounds.x && pos.y >= minBounds.y && pos.z >= minBounds.z &&
-         pos.x <= maxBounds.x && pos.y <= maxBounds.y && pos.z <= maxBounds.z;
+  pos.x <= maxBounds.x && pos.y <= maxBounds.y && pos.z <= maxBounds.z;
 }
 
 void main() {
-  // compute vector to camera in texture space  
   vec3 rayDirectionInTextureSpace = normalize(entryPoint-cameraPosInTextureSpace);
 
   // compute delta
+  vec3 voxelCount = vec3(textureSize(volume,0));
   float samples = dot(abs(rayDirectionInTextureSpace),voxelCount);
   float opacityCorrection = 100/(samples*oversampling);
   vec3 delta = rayDirectionInTextureSpace/(samples*oversampling);
 
   vec3 currentPoint = entryPoint;
-  result = vec4(0.0);
-  do {
-    // TODO: Implement raycaster and store the result in "result".
-    //       The next two lines are placeholder code ensuring all
-    //       variables are utilized. This prevents exceptions from
-    //       the initial shader. These placeholder lines should be
-    //       replaced by the raycasting code.
-    result = 1+transferFunction(texture(volume,delta).r);
-    break;
-  } while (inBounds(currentPoint));
+
+  // TODO: add raycaster here
+  result = vec4(1.0);
 }
